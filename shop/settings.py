6f023095555662c013dec813e27fec328131f6e7
Vlad Radchenko <specforworkwithpeop@gmail.com>
2021-02-05 10:21:23 +0300

    Return secret key

--- shop/settings.py
+++ shop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = '&q2b4wl%^v6_$p*afp)ga+volkecx8o!v!q*yg%rm&kk@6%zh_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,7 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'specforworkwithpeop@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_PASSWORD = 'izmnfmlxwleytteg'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Mini shop's admin"
